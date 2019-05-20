72a2b7cdac80a957d3e6da5a5c34a19cb2fba8a2
Jonathan Howard <jonathanhoward@localhost.localdomain>
2019-05-20 18:49:09 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           DIFFERENT A THIRD TIME?! OMG OMG!
+           Continuing to get the hang of Docker!!!
         </p>
         <a
           className="App-link"
